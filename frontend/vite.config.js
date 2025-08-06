7533396960ab2ba02cb6a46e87cf537487acaaa3
choi0712 <130032473+ggnu11@users.noreply.github.com>
2025-08-06 14:43:00 +0900

    수정

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,10 +7,19 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
     outDir: "dist",
-    assetsDir: "assets",
     rollupOptions: {
       output: {
         manualChunks: undefined,
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name.split(".");
+          const ext = info[info.length - 1];
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(ext)) {
+            return `assets/images/[name]-[hash][extname]`;
+          }
+          return `assets/[name]-[hash][extname]`;
+        },
+        chunkFileNames: "assets/js/[name]-[hash].js",
+        entryFileNames: "assets/js/[name]-[hash].js",
       },
     },
   },
