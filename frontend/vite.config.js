fec587b1ace8aec3d15fc0a73674502e5acfeb7a
choi0712 <130032473+ggnu11@users.noreply.github.com>
2025-08-06 14:37:57 +0900

    수정

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "./",
   plugins: [react(), tailwindcss()],
   server: {
     allowedHosts: ["ac6e9857ba60.ngrok-free.app"],
